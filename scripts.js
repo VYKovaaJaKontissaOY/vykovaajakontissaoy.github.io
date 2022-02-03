5c215a5714d214631842b3f3e914f476d457d5c6
Teemu <teemu.jarvinen97@gmail.com>
2022-02-03 15:19:49 +0200

    deleted unneeded

--- scripts.js
+++
@@ -1,8 +0,0 @@
-var modalBtn = document.getElementById("nav-contact");
-var modal = document.querySelector(".modal");
-
-modalBtn.addEventListener("click", openModal);
-
-function openModal() {
-  modal.style.display = "block";
-}
